cdf235f9327c22038d2c33447aa023519080868c
Alex <alszznt@gmail.com>
2022-07-21 12:53:03 +0300

    feat(ui): breeds list item

--- src/components/breeds-image-list-item/breeds-image-list-item.js
+++ src/components/breeds-image-list-item/breeds-image-list-item.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
+import { ImageErrorIndicator } from '../error-indicators';
+
 import './breeds-image-list-item.css';
 
 const BreedsImageListItem = ({ props }) => {
 
-    console.log(props);
+    console.log(props.name);
 
     return(
         <div className = "breeds-image-list-item">
-            <div 
-                // style = {{ backgroundImage:`url(${ url })`}}
-                className = "breeds-image-list-item-image"
-            />
+            { props.image === undefined ? <ImageErrorIndicator /> : <Image url = { props.image.url }/> }
+            <div className = "breeds-image-list-item-overlay">
+                <div className = "breeds-image-list-item-name">
+                    { props.name }
+                </div>
+            </div>
         </div>
+    )
+}
+
+const Image = ({ url }) => {
+    return(
+        <div 
+            style = {{ backgroundImage:`url(${ url })`}}
+            className = "breeds-image-list-item-image"
+        />
     )
 }
 
